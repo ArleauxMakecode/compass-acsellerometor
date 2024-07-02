69c19dfa7aa8ee95f209106e8115648fb5d36ce9
ArleauxMakecode <120610871+ArleauxMakecode@users.noreply.github.com>
2024-07-02 16:54:27 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
+let rf = 0
 let deGReeS = 0
+input.onButtonPressed(Button.A, function () {
+    rf = 1
+})
+buttonClicks.onButtonUp(buttonClicks.AorB.A, function () {
+    rf = 0
+})
 basic.forever(function () {
     deGReeS = input.compassHeading()
     if (deGReeS < 45) {
@@ -11,5 +18,10 @@ basic.forever(function () {
         basic.showString("W")
     } else {
         basic.showString("N")
+    }
+})
+basic.forever(function () {
+    if (rf == 1) {
+        led.plot(input.acceleration(Dimension.X), input.acceleration(Dimension.Y))
     }
 })
